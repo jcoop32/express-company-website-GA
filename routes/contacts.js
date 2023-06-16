e98480268b6c2ea66eeed0c496710319cc2b1a54
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-16 13:55:13 -0500

    contact page added

---
+++ routes/contacts.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+//route -> /companyHistory
+router.get('/', function (req, res, next) {
+  res.render('contacts/index', {
+    title: 'Contact',
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var companyHistoryRouter = require('./routes/companyHistory');
 var pastWorksRouter = require('./routes/pastWorks');
 var staffsRouter = require('./routes/staffs');
+var contactsRouter = require('./routes/contacts');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/companyHistory', companyHistoryRouter);
 app.use('/pastWorks', pastWorksRouter);
 app.use('/staffs', staffsRouter);
+app.use('/contacts', contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
